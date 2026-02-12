424f6a49fa62ca513367cec81bc9ec75013852a5
Mano <manojkumararumainathan@gmail.com>
2026-02-12 16:40:20 +0530

    Enable middleware

--- proxy.ts
+++ proxy.ts
@@ -5,21 +5,21 @@ export function proxy(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken')?.value;
   const { pathname } = request.nextUrl;
 
-  // if (
-  //   pathname.startsWith('/_next') ||
-  //   pathname.startsWith('/api') ||
-  //   pathname.includes('.')
-  // ) {
-  //   return NextResponse.next();
-  // }
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/api') ||
+    pathname.includes('.')
+  ) {
+    return NextResponse.next();
+  }
 
-  // if (!accessToken && pathname !== '/login') {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+  if (!accessToken && pathname !== '/login') {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
 
-  // if (accessToken && pathname === '/login') {
-  //   return NextResponse.redirect(new URL('/dashboard', request.url));
-  // }
+  if (accessToken && pathname === '/login') {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
 
   return NextResponse.next();
 }
